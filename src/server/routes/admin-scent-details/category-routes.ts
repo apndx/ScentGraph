97d4eb39df5af75a2575884b8c6023b72749f501
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2023-04-07 23:50:57 +0300

    Refactor category routes to use neo4j driver and session

--- src/server/routes/admin-scent-details/category-routes.ts
+++ src/server/routes/admin-scent-details/category-routes.ts
@@ -1,12 +1,12 @@
 import * as express from 'express'
+import * as neo4j from 'neo4j-driver'
 import { checkAdmin, checkLogin } from '../../middleware'
-import { category } from '../../models'
 import { convertToScentItem } from '../route-helpers'
 import { ScentItem } from '../../../common/data-classes'
 
 export function configureCategoryRoutes(
   app: express.Application,
-  instance: any,
+  driver: neo4j.Driver,
   apiPath: string
 ): void {
   const ADMIN_DETAILS_PATH = `${apiPath}/category`
@@ -15,28 +15,36 @@ export function configureCategoryRoutes(
     `${ADMIN_DETAILS_PATH}/add`, checkLogin,
     async (req: express.Request, res: express.Response) => {
 
-      instance.model('Category', category)
+      const session = driver.session()
 
       try {
 
-        const existingCategory = await instance.cypher('MATCH (category:Category {categoryname:$itemName}) return category.categoryname', req.body)
+        const getCategoryCypher = 'MATCH (category:Category {categoryname:$itemName}) return category.categoryname'
+        const existingCategory = await session.run(getCategoryCypher, { itemName: req.body.itemName })
+
         if (existingCategory.records.length > 0) {
           return res.status(400).json({ error: 'Category must be unique.' })
         }
 
         Promise.all([
-          instance.create('Category', {
-            categoryname: req.body.itemName,
-            label: req.body.label
-          })
+          session.run(`
+            CREATE (category:Category {
+              categoryname: $categoryName,
+              label: $label
+            })
+            RETURN category
+            `,
+            { categoryName: req.body.itemName,
+              label: req.body.label }
+          )
         ])
           .then(([category]) => {
-            res.status(200).send(`Category ${category.properties().categoryname} created`)
+            res.status(200).send(`Category ${req.body.itemName} created`)
           })
           .catch((e: any) => {
             console.log('Error :(', e, e.details) // eslint-disable-line no-console
           })
-          .then(() => instance.close())
+          .then(() => session.close())
       } catch (e) {
         console.log(e)
         res.status(500).json({ error: 'Something went wrong in creating a category' })
@@ -48,11 +56,14 @@ export function configureCategoryRoutes(
     `${ADMIN_DETAILS_PATH}/all`,
     async (req: express.Request, res: express.Response) => {
 
-      instance.model('Category', category)
+      const session = driver.session()
+
       const categories: ScentItem[] = []
+
       try {
-        const result = await instance.cypher('MATCH (category:Category) RETURN category')
-          .then((result: any) => {
+        const getCategoriesCypher = 'MATCH (category:Category) RETURN category'
+        session.run(getCategoriesCypher)
+          .then((result: neo4j.QueryResult) => {
             result.records.map((row: any) => {
               categories.push(convertToScentItem(row.get('category')))
             })
@@ -62,7 +73,7 @@ export function configureCategoryRoutes(
           .catch((e: any) => {
             console.log('Error :(', e, e.details) // eslint-disable-line no-console
           })
-          .then(() => instance.close())
+          .then(() => session.close())
       } catch (e) {
         console.log(e)
         res.status(500).json({ error: 'Something went wrong when fetching categories' })
@@ -74,15 +85,18 @@ export function configureCategoryRoutes(
     `${ADMIN_DETAILS_PATH}/delete`, checkAdmin,
     async (req: express.Request, res: express.Response) => {
 
+      const session = driver.session()
+
       try {
-        await instance.cypher('MATCH (category:Category {categoryname:$itemName}) DELETE category', req.body)
+        const deleteCategoryCypher = 'MATCH (category:Category {categoryname:$itemName}) DELETE category'
+        await session.run(deleteCategoryCypher, req.body)
           .then(() => {
             res.status(200).send('Category deleted')
           })
           .catch((e: any) => {
             console.log('Error :(', e, e.details) // eslint-disable-line no-console
           })
-          .then(() => instance.close())
+          .then(() => session.close())
       } catch (e) {
         console.log(e)
         res.status(500).json({ error: 'Something went wrong in deleting a category' })
