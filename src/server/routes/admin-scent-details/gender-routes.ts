06d99d998fcc99a05c6af9166e243ac2ccd4c835
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2023-04-09 13:28:34 +0300

    Refactor gender to use neo4j driver and session

--- src/server/routes/admin-scent-details/gender-routes.ts
+++ src/server/routes/admin-scent-details/gender-routes.ts
@@ -1,12 +1,12 @@
 import * as express from 'express'
+import * as neo4j from 'neo4j-driver'
 import { checkAdmin } from '../../middleware'
-import { gender } from '../../models'
 import { ScentItem } from '../../../common/data-classes'
 import { convertToScentItem } from '../route-helpers'
 
 export function configureGenderRoutes(
   app: express.Application,
-  instance: any,
+  driver: neo4j.Driver,
   apiPath: string
 ): void {
   const ADMIN_DETAILS_PATH = `${apiPath}/gender`
@@ -15,27 +15,35 @@ export function configureGenderRoutes(
     `${ADMIN_DETAILS_PATH}/add`, checkAdmin,
     async (req: express.Request, res: express.Response) => {
 
-      instance.model('Gender', gender)
+      const session = driver.session()
 
       try {
 
-        const existingGender = await instance.cypher('MATCH (gender:Gender {gendername:$itemName}) return gender.gendername', req.body)
+        const getGenderCypher = 'MATCH (gender:Gender {gendername:$itemName}) return gender.gendername'
+        const existingGender = await session.run(getGenderCypher, { itemName: req.body.itemName })
         if (existingGender.records.length > 0) {
           return res.status(400).json({ error: 'Gender must be unique.' })
         }
 
         Promise.all([
-          instance.create('Gender', {
-            gendername: req.body.itemName
-          })
+          session.run(`
+            CREATE (gender:Gender {
+              gendername: $genderName,
+              label: $label
+            })
+            RETURN gender
+            `,
+            { genderName: req.body.itemName,
+              label: req.body.label }
+          )
         ])
-          .then(([gender]) => {
-            res.status(200).send(`Gender ${gender.properties().gendername} created`)
+          .then(() => {
+            res.status(200).send(`Gender ${req.body.itemName} created`)
           })
           .catch((e: any) => {
             console.log('Error :(', e, e.details) // eslint-disable-line no-console
           })
-          .then(() => instance.close())
+          .then(() => session.close())
       } catch (e) {
         console.log(e)
         res.status(500).json({ error: 'Something went wrong in creating a gender' })
@@ -47,11 +55,12 @@ export function configureGenderRoutes(
     `${ADMIN_DETAILS_PATH}/all`,
     async (req: express.Request, res: express.Response) => {
 
-      instance.model('Gender', gender)
+      const session = driver.session()
       const genders: ScentItem[] = []
       try {
-        const result = await instance.cypher('MATCH (gender:Gender) RETURN gender')
-          .then((result: any) => {
+        const getGendersCypher = 'MATCH (category:Category) RETURN category'
+        session.run('MATCH (gender:Gender) RETURN gender')
+          .then((result: neo4j.QueryResult) => {
             result.records.map((row: any) => {
               genders.push(convertToScentItem(row.get('gender')))
             })
@@ -61,7 +70,7 @@ export function configureGenderRoutes(
           .catch((e: any) => {
             console.log('Error :(', e, e.details) // eslint-disable-line no-console
           })
-          .then(() => instance.close())
+          .then(() => session.close())
       } catch (e) {
         console.log(e)
         res.status(500).json({ error: 'Something went wrong when fetching genders' })
@@ -72,16 +81,16 @@ export function configureGenderRoutes(
   app.delete(
     `${ADMIN_DETAILS_PATH}/delete`, checkAdmin,
     async (req: express.Request, res: express.Response) => {
-
+      const session = driver.session()
       try {
-        await instance.cypher('MATCH (gender:Gender {gendername:$itemName}) DELETE gender', req.body)
+        session.run('MATCH (gender:Gender {gendername:$itemName}) DELETE gender', req.body.itemName)
           .then(() => {
             res.status(200).send('Gender deleted')
           })
           .catch((e: any) => {
             console.log('Error :(', e, e.details) // eslint-disable-line no-console
           })
-          .then(() => instance.close())
+          .then(() => session.close())
       } catch (e) {
         console.log(e)
         res.status(500).json({ error: 'Something went wrong in deleting a gender' })
